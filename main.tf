eb05ddeb22df2290543bbda88331acf214ba0740
David-Nettleship <david.nettleship1@gmail.com>
2025-08-16 11:19:52 +0100

    Enable public access to S3

--- main.tf
+++ main.tf
@@ -12,6 +12,15 @@ resource "aws_s3_bucket" "website_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.website_bucket.id
 
